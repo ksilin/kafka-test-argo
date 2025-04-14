60c5e4f24ea90af99a75a01a0b0e3d7e9cf975f1
Benedikt Linse <benedikt@confluent.io>
2025-04-14 09:26:32 +0000

    upgrading azure image version to 0.9

--- build-with-azure.sh
+++ build-with-azure.sh
@@ -1,3 +1,3 @@
 ACR_NAME=benediktubuntudev
-az acr build --registry $ACR_NAME --image kafka_e2e_test:v0.8 --file docker/Dockerfile docker
+az acr build --registry $ACR_NAME --image kafka_e2e_test:v0.9 --file docker/Dockerfile docker
 
